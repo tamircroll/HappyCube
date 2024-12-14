186b05f643ba4a63564f0d815c41c3a0e9367f88
tamirc <tamirc@accessfintech.com>
2024-12-14 16:41:48 +0200

    Merge remote-tracking branch 'origin/master'

--- src/HappyCube/CubeValidator.java
+++ src/HappyCube/CubeValidator.java
@@ -36,8 +36,6 @@ public class CubeValidator {
                         if(!calculate(fixedFaces, facesRest)) {
                             fixedFaces.remove(currentFace);
                             facesRest.add(faceIdx, currentFace);
-                        } else {
-//                            return true;
                         }
                     }
                     currentFace.rotate();
